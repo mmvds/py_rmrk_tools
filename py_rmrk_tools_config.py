aede6dbc0984ac8a8eddee78893a316ebcf04a3e
Victor Ryabinin <rybvic@gmail.com>
2022-02-02 15:00:19 +0300

    Update py_rmrk_tools_config.py

--- py_rmrk_tools_config.py
+++ py_rmrk_tools_config.py
@@ -25,6 +25,9 @@ fee_dict = {'1.0.0': singular_fee, '2.0.0': kanaria_fee}
 # emote nft extrinsics versions
 emote_version_dict = {'1.0.0': '1.0.0', '2.0.0': '2.0.0::RMRK2'}
 
+# mint nft events
+mintntf_operation_dict = {'1.0.0': 'MINTNFT', '2.0.0': 'MINT'}
+
 # postgres db (if you use rmrk2psql)
 pg_login = "postgres"
 pg_pass = "postpass"
